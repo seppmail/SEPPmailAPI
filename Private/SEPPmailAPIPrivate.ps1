9e5fa96b0822293ca35529c95d2368c051b623b5
stadlmair <stadlmair@seppmail.at>
2021-03-06 17:35:03 +0100

    Remove-SMAcsutomer works now with parameters

--- Private/SEPPmailAPIPrivate.ps1
+++ Private/SEPPmailAPIPrivate.ps1
@@ -29,6 +29,44 @@ function New-SMAUrlRoot {
     }
 }
 
+function New-SMAQueryString {
+    [CmdletBinding()]
+    param 
+    (
+        [Parameter(Mandatory = $true)]
+        [String]$Uri,
+
+        [Parameter(Mandatory = $true)]
+        [String]$path,
+
+        [Parameter(Mandatory = $true)]
+        [int]$Port = 8445,
+
+        [Parameter(Mandatory = $true)]
+        [Hashtable]$QueryParameter
+
+
+    )
+
+    # Add System.Web
+    Add-Type -AssemblyName System.Web
+
+    # Create a http name value collection from an empty string
+    $nvCollection = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+
+    foreach ($key in $QueryParameter.Keys)
+    {
+        $nvCollection.Add($key, $QueryParameter.$key)
+    }
+
+    # Build the uri
+    $uriRequest = [System.UriBuilder]$uri
+    $uriRequest.Query = $nvCollection.ToString()
+
+    return $uriRequest.Uri.OriginalString
+}
+
+
 function ConvertFrom-SMAPIFormat {
     [CmdletBinding()]
     param (
